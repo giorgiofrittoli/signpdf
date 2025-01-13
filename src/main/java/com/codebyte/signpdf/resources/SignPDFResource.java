97402ef3cece4bd1737f793ef40e405d73c1699a
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2025-01-13 22:38:27 +0100

    fix logo

--- src/main/java/com/codebyte/signpdf/resources/SignPDFResource.java
+++ src/main/java/com/codebyte/signpdf/resources/SignPDFResource.java
@@ -76,18 +76,18 @@ public class SignPDFResource {
 
             String pdfID = RandomStringUtils.random(13, true, true).toUpperCase();
 
-            BufferedImage image = new BufferedImage(250, 33, BufferedImage.TYPE_INT_ARGB);
+            BufferedImage image = new BufferedImage(250, 70, BufferedImage.TYPE_INT_ARGB);
             Graphics g = image.getGraphics();
-            g.drawImage(ImageIO.read(SignPDFResource.class.getResource("/cb.png")), 0, 0, null);
-            g.setFont(g.getFont().deriveFont(8f));
+            g.drawImage(ImageIO.read(SignPDFResource.class.getResource("/cob.jpg")), 0, 0, null);
+            g.setFont(g.getFont().deriveFont(10f));
             g.setColor(Color.BLACK);
-            g.drawString("Certified Firmapdf.net", 55, 8);
-            g.drawString("Signer " + newSignRequest.getCaller(), 55, 16);
-            g.drawString("Date " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("y-M-d H:m:s")), 55, 24);
-            g.drawString("PDFId " + pdfID, 55, 32);
+            g.drawString("Certified Firmapdf.net", 55, 10);
+            g.drawString("Signer " + newSignRequest.getCaller(), 55, 20);
+            g.drawString("Date " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("y-M-d H:m:s")), 55, 30);
+            g.drawString("PDFId " + pdfID, 55, 40);
             g.dispose();
 
-            File logoForSign = new File(FileUtils.getTempDirectory(), UUID.randomUUID() + ".png");
+            File logoForSign = new File("/Users/giorgio/Downloads/", UUID.randomUUID() + ".png");
             ImageIO.write(image, "png", logoForSign);
 
             String md5Logo;
@@ -110,7 +110,7 @@ public class SignPDFResource {
                         // better method inspired by http://stackoverflow.com/a/22318681/535646
                         // reduce this value if the image is too large
                         float scale = 1f;
-                        contentStream.drawImage(pdImage, 0, 800, pdImage.getWidth() * scale, pdImage.getHeight() * scale);
+                        contentStream.drawImage(pdImage, 0, 770, 250, 70);
                     }
                 }
 
